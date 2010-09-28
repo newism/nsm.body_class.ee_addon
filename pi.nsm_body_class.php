29a318f7463d9eeb8a82de8080ba5cb9f1824f2e
Daniel Ott <github@danott.us>
2010-09-28 14:23:23 -0400

    Added EE1 Compatability

--- pi.nsm_body_class.php
+++ pi.nsm_body_class.php
@@ -1,4 +1,5 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php /* if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+			/* @danott fork. Commented this out for EE1 compatibility */
 
 /**
  * NSM Body Class Plugin
@@ -6,7 +7,7 @@
  * Generally a module is better to use than a plugin if if it has not CP backend
  *
  * @package			NsmBodyClass
- * @version			1.0.0
+ * @version			1.0.1
  * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
  * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
  * @license 		Commercial - please see LICENSE file included with this distribution
@@ -14,6 +15,15 @@
  * @see 			http://expressionengine.com/public_beta/docs/development/plugins.html
  */
 
+/*
+ * @danott Fork
+ *
+ * Forked by @danott to bring compatability to an EE1 project.
+ * The basic strategy is simple. Abstract the TMPL class using @erikreagan's technique from
+ * Dan Benjamin's Shrimp plugin. Then replace all calls to $this->EE->TMPL->function() with
+ * $this->TMPL->function().
+ */
+ 
 /**
  * Plugin Info
  *
@@ -21,7 +31,7 @@
  */
 $plugin_info = array(
 	'pi_name' => 'NSM Body Class',
-	'pi_version' => '1.0.0',
+	'pi_version' => '1.0.1',
 	'pi_author' => 'Leevi Graham &mdash; Technical Director, <a href="http://newism.com.au/">Newism</a>',
 	'pi_author_url' => 'http://leevigraham.com/',
 	'pi_description' => 'Returns a class attribute or string based on embedded variables',
@@ -29,6 +39,14 @@ $plugin_info = array(
 );
 
 class Nsm_body_class {
+	
+	/**
+	 * The ExpressionEngine TMPL object, independent of version
+	 * Implemented in @danott fork
+	 *
+	 * @var object
+	 **/
+	protected $TMPL;
 	
 	/**
 	 * The return data
@@ -53,16 +71,18 @@ class Nsm_body_class {
 	 * @var array
 	 **/
 	protected $param_map = array(
-		"entry_id" 			=> "eid-",
-		"url_title" 		=> "eut-",
-		"year"				=> "y-",
-		"month"				=> "m-",
-		"day"				=> "d-",
-		"template" 			=> "t-",
-		"template_group" 	=> "tg-",
+		"entry_id"        => "eid-",
+		"url_title" 		  => "eut-",
+		"year"            => "y-",
+		"month"           => "m-",
+		"day"             => "d-",
+		"template"        => "t-",
+		"template_group"  => "tg-",
 		"channel_name"		=> "cn-",
-		"channel_id"		=> "cid-",
-		"body_class"		=> ""
+		"channel_id"	  	=> "cid-",
+		"weblog_name"		  => "wn-",   // @danott fork. Include both EE1 and EE2 nomenclatures since only what is passed shows up anyway.
+		"weblog_id"	    	=> "wid-",  // @danott fork. Include both EE1 and EE2 nomenclatures since only what is passed shows up anyway.
+		"body_class"	  	=> ""
 	);
 
 	/**
@@ -73,18 +93,33 @@ class Nsm_body_class {
 	 **/
 	public function nsm_body_class()
 	{
-		$this->EE =& get_instance();
-		$this->_extendParamMap();
+	  
+		/* 
+		 * @danott fork
+		 * Technique from @erikreagan in Dan Benjamin's Shrimp plugin
+		 * EE version check to properly reference our EE objects
+		 */
+		if (version_compare(APP_VER, '2', '<'))
+		{
+			// EE 1.x is in play
+			global $TMPL;
+			$this->TMPL =& $TMPL;
+		} else {
+			// EE 2.x is in play
+			$this->EE	=& get_instance();
+			$this->TMPL =& $this->EE->TMPL;
+		}
 
-		if(! $return = $this->EE->TMPL->fetch_param("return"))
+
+		if(! $return = $this->TMPL->fetch_param("return"))
 			$retun = "class";
 
 		foreach ($this->param_map as $var => $prefix)
 		{
-			if(array_key_exists("embed:" . $var, $this->EE->TMPL->embed_vars))
+			if(array_key_exists("embed:" . $var, $this->TMPL->embed_vars))
 			{
-				if(! $val = $this->EE->TMPL->fetch_param($var))
-					$val = $this->EE->TMPL->embed_vars["embed:" . $var];
+				if(! $val = $this->TMPL->fetch_param($var))
+					$val = $this->TMPL->embed_vars["embed:" . $var];
 
 				$this->class_string .= "{$prefix}{$val} ";
 			}
@@ -99,12 +134,12 @@ class Nsm_body_class {
 	 **/
 	private function _extendParamMap()
 	{
-		if(! $param_map = $this->EE->TMPL->fetch_param("param_map"))
+		if(! $param_map = $this->TMPL->fetch_param("param_map"))
 			return;
 		
 		$param_map = explode("|", $param_map);
 
-		if(!$this->EE->TMPL->fetch_param("replace_param_map"))
+		if(!$this->TMPL->fetch_param("replace_param_map"))
 			$this->param_map = array();
 
 		foreach ($param_map as $prop)
